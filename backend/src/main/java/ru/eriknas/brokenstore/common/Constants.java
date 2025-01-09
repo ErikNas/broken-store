bac1c22900efcb773948f1fd402688066160ef8b
Маркин Дмитрий Сергеевич <dmitriy.markin@rtkit.ru>
2025-01-09 12:39:21 +0300

    [BRSTORE-16] fix error msg

--- backend/src/main/java/ru/eriknas/brokenstore/common/Constants.java
+++ backend/src/main/java/ru/eriknas/brokenstore/common/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 
     //error-message
     public static final String ID_MUST_BE_INTEGER = "id должен быть целочисленным значением";
-    public static final String INVALID_PAGE = "Параметр \"page\" должен быть ⩾ 0";
-    public static final String INVALID_SIZE = "Параметр \"size\" должен быть > 0";
+    public static final String INVALID_PAGE = "Параметр 'page' должен быть ⩾ 0";
+    public static final String INVALID_SIZE = "Параметр 'size' должен быть > 0";
     public static final String ORDER_NOT_FOUND = "Заказ с id=%s не найден";
     public static final String TSHIRT_NOT_FOUND = "Футболка с id=%s не найдена";
     public static final String USER_NOT_FOUND = "Пользователь с id=%s не найден";
