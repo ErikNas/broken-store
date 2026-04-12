f1879d5df2f8d4a1480d2a2207cbaca12d8ca903
Stanislav Bartosh <Stanislav.Bartosh@rt.ru>
2026-04-12 15:49:52 +0300

    Уменьшена чувствительность при проверке текста на нецензурную лексику.

--- backend-python/profanity-validator-service/profanity-validator-controller.py
+++ backend-python/profanity-validator-service/profanity-validator-controller.py
@@ -9,7 +9,7 @@ def validate_profanity():
     data = request.get_json()
     if data:
         phrase = data.get('phrase')
-        predict = ru_profanity_validator.predict(phrase, 0.2)[0]
+        predict = ru_profanity_validator.predict(phrase, 0.5)[0]
         predict_proba = ru_profanity_validator.predict_proba(phrase)[0]
         return jsonify({"predict": predict, "predict_proba": predict_proba})
     else:
